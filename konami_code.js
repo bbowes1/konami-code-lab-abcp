37670c27cf93899a3fbf61532ac3d09e187ef8bf
Benjamin Bowes <bbowes999@gmail.com>
2018-01-30 11:27:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0
 function init() {
 }
-  body.addEventListener('keydown', function(e) {
+  body.addEventListener('keydown', 
+  function Konamicode(e) {
   const key = parseInt(e.detail || e.which);
 
    if (key === code[index]) {
